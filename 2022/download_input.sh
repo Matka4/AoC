776cbefc777b87a7fdb7d05de207126aff2d9778
Davchev, Dushko <dushko.davchev@scalefocus.com>
2022-12-02 09:14:17 +0100

    input session token from .env file

--- 2022/download_input.sh
+++ 2022/download_input.sh
@@ -4,10 +4,12 @@ echo "Insert day number: "
 read -e day_number
 echo "-------------------------------------------------------------"
 
-aoc_token="<your token here>"
+# Put session_token=foobar in your .env file
+. ".env"
+aoc_session=$session_token
 
 echo " The input.txt file has been downloaded into day$day_number directory"
 
 echo "-------------------------------------------------------------"
 
-curl -b "session=$aoc_token" https://adventofcode.com/2022/day/$day_number/input 2>/dev/null > day$day_number/input_test.txt
+curl -b "session=$aoc_session" https://adventofcode.com/2022/day/$day_number/input 2>/dev/null > day$day_number/input_test.txt
